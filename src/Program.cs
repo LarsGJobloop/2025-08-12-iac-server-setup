9b3f24c7ffcf1c1195f9775702d73c16a1a9ba05
zabronax <zabronax@gmail.com>
2025-08-14 11:10:11 +0200

    listen on all interfaces. Not recommended

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.ConfigureKestrel(serverOptions =>
+{
+  serverOptions.ListenAnyIP(5000); // HTTP on all interfaces
+});
+
 var app = builder.Build();
-
 app.MapGet("/", () => "Hello World!");
-
 app.Run();
