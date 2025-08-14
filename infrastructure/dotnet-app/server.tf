6685a6a6f38f9eb956335ec9428f685411730ff7
zabronax <zabronax@gmail.com>
2025-08-14 09:56:36 +0200

    fix module path error

--- infrastructure/dotnet-app/server.tf
+++ infrastructure/dotnet-app/server.tf
@@ -1,17 +1,17 @@
 resource "hcloud_server" "server" {
-  name = "lecture-server"
-  location = "hel1" # Helsinki, Finland
+  name        = "lecture-server"
+  location    = "hel1" # Helsinki, Finland
   server_type = "cx22" # 2 vCPU, 4GB RAM, 40GB SSD
-  image = "debian-12"
+  image       = "debian-12"
 
-  user_data = file("cloud-config.yaml")
+  user_data = file("${path.module}/cloud-config.yaml")
 
   ssh_keys = [
-    hcloud_ssh_key.server_key.id 
+    hcloud_ssh_key.server_key.id
   ]
 }
 
 output "server_detail" {
   description = "All the server details"
-  value = hcloud_server.server
+  value       = hcloud_server.server
 }
